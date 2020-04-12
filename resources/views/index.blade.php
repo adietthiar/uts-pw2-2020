3ca18c95a2ea516980459b089b4278316a7cf830
adietthiar <adiet1999@student.untan.ac.id>
2020-04-12 10:22:41 +0700

    Pengumpulan uts pw2

---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layout.master')
+
+@section('content')
+<body>
+    <div class="app-container app-theme-white body-tabs-shadow fixed-sidebar fixed-header">
+    @include('layout.navigation')
+               <div class="ui-theme-settings">
+            <button type="button" id="TooltipDemo" class="btn-open-options btn btn-warning">
+                <i class="fa fa-cog fa-w-16 fa-spin fa-2x"></i>
+            </button>
+
+        </div>        <div class="app-main">
+                @include('layout.sidebar')
+                    <div class="app-main__outer">
+                @include ('layout.index')
+            </div>
+        </div>
+    </div>
+<script type="text/javascript" src="./assets/scripts/main.js"></script></body>
+</html>
